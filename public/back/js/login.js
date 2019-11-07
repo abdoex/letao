de69a398fba6b33660fde15f62174bfee19429c0
abdoex <782636516@qq.com>
2019-11-07 17:57:04 +0800

    ajax提交功能实现

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -2,8 +2,9 @@ $(function () {
 
 //    对表单进行校验
     var btn = $("#btn_submit");
+    var form = $("#login_form");
 
-    $("#login_form").bootstrapValidator({
+    form.bootstrapValidator({
         feedbackIcons: {
             valid: 'glyphicon glyphicon-ok',
             invalid: 'glyphicon glyphicon-remove',
@@ -15,8 +16,11 @@ $(function () {
                 validators: {
                     notEmpty: {
                         message: "用户名不能为空"
+                    },
+                    callback: {
+                        message: "用户名不存在",
                     }
-                }
+                },
             },
             password: {
                 validators: {
@@ -27,10 +31,45 @@ $(function () {
                         min: 6,
                         max: 12,
                         message: '用户名长度必须在6到12之间'
+                    },
+                    callback: {
+                        message:"密码错误",
                     }
                 }
             }
         }
+    })
+
+    //点击提交后对数据进行处理
+    form.on('success.form.bv', function (e) {
+        e.preventDefault();
+        //使用ajax提交逻辑
+
+        $.ajax({
+            type: "post",
+            url: "/employee/employeeLogin",
+            data: form.serialize(),
+            success: function (info) {
+                console.log(info);
+                if (info.success) {
+                    location.href = "./index1.html";
+                }
+                //    要添加判错提示
+                if(info.error==1000){
+                    form.data('bootstrapValidator').updateStatus('username', 'INVALID', 'callback')
+                }
+                if(info.error==1001){
+                    form.data('bootstrapValidator').updateStatus('password', 'INVALID', 'callback')
+                }
+
+            }
+        })
+
+    });
+
+//   重置表单
+    $("#form_reset").click(function(){
+        form.data('bootstrapValidator').resetForm();
     })
 
 })
